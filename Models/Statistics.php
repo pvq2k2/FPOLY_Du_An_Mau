bead233a9e9499166a5f01406700b2b5db561b0e
pvq2k2 <vanquyet2392002@gmail.com>
2023-07-24 00:32:11 +0700

    refactor: Statistics.php

--- Models/Statistics.php
+++ Models/Statistics.php
@@ -2,11 +2,11 @@
 
 /**
  * Thống kê tài khoản bình luận nhiều nhất 
- * @return array trả về một mảng gồm (tai_khoan_id, ho_va_ten, so_luong_binh_luan)
+ * @return array trả về một mảng gồm (tai_khoan_id, ho_va_ten, hinh, so_luong_binh_luan)
  */
 function MostCommentedAccountStatistics()
 {
-    $sql = "SELECT BL.tai_khoan_id, TK.ho_va_ten, COUNT(*) AS so_luong_binh_luan
+    $sql = "SELECT BL.tai_khoan_id, TK.ho_va_ten, TK.hinh, COUNT(*) AS so_luong_binh_luan
     FROM binh_luan AS BL
     INNER JOIN tai_khoan AS TK ON TK.tai_khoan_id = BL.tai_khoan_id
     GROUP BY tai_khoan_id
@@ -32,7 +32,7 @@ function MostCommentedProductStatistics()
 
 /**
  * Thống kê tổng số lượng bình luận
- * @return int trả về số lượng bình luận
+ * @return array trả về một mảng gồm (so_luong_binh_luan)
  */
 function StatisticsOfTheTotalNumberOfComments()
 {
@@ -42,11 +42,11 @@ function StatisticsOfTheTotalNumberOfComments()
 
 /**
  * Thống kê sản phẩm có số lượng xem nhiều nhất
- * @return int trả về số lượng bình luận
+ * @return array trả về một mảng gồm (ten_san_pham, gia, hinh, so_luot_xem)
  */
 function ProductStatisticsWithTheMostViews()
 {
-    $sql = "SELECT san_pham_id, ten_san_pham, so_luot_xem 
+    $sql = "SELECT ten_san_pham, gia, hinh, so_luot_xem 
     FROM san_pham
     ORDER BY so_luot_xem DESC
     LIMIT 1";
@@ -70,22 +70,22 @@ function StatisticsOfProductsWithTheLatestImportDate()
 
 /**
  * Thống kê sản phẩm được mua nhiều nhất
- * @return array trả về một mảng gồm (san_pham_id, ten_san_pham, so_luot_mua)
+ * @return array trả về một mảng gồm (ten_san_pham, gia, hinh, so_luot_mua)
  */
 function MostPurchasedProductStatistics()
 {
-    $sql = "SELECT SP.san_pham_id, SP.ten_san_pham, COUNT(*) so_luong_mua 
+    $sql = "SELECT SP.ten_san_pham, SP.gia, SP.hinh, COUNT(*) so_luot_mua 
     FROM chi_tiet_don_hang AS CTDH
-    INNER JOIN san_pham AS SP ON SP.san_phan_id = CTDH.san_pham_id
-    GROUP BY SP.san_pham_id, SP.ten_san_pham
-    ORDER BY so_luong_mua DESC
+    LEFT JOIN san_pham AS SP ON SP.san_pham_id = CTDH.san_pham_id
+    GROUP BY SP.ten_san_pham, SP.gia, SP.hinh
+    ORDER BY so_luot_mua DESC
     LIMIT 1";
     return pdo_query($sql);
 }
 
 /**
  * Thống kê số lượng người dùng
- * @return int trả về số lượng người dùng
+ * @return array trả về một mảng gồm (so_luong_nguoi_dung)
  */
 function StatisticsOnTheNumberOfUsers()
 {
@@ -95,7 +95,7 @@ function StatisticsOnTheNumberOfUsers()
 
 /**
  * Thống kê số lượng đơn hàng
- * @return int trả về số lượng đơn hàng
+ * @return array trả về một mảng gồm (so_luong_don_hang)
  */
 function OrderQuantityStatistics()
 {
@@ -105,7 +105,7 @@ function OrderQuantityStatistics()
 
 /**
  * Thống kê tổng doanh thu
- * @return int trả về tổng doanh thu
+ * @return array trả về một mảng gồm (tong_doanh_thu)
  */
 function TotalRevenueStatistics()
 {
@@ -115,13 +115,13 @@ function TotalRevenueStatistics()
 
 /**
  * Thống kê số lượng sản phẩm trong danh mục
- * @return array trả về danh sách mảng gồm (san_pham_id, so_luong_san_pham)
+ * @return array trả về danh sách mảng gồm (ten_danh_muc, so_luong_san_pham)
  */
 function StatisticsOnTheNumberOfProductsInTheCategory()
 {
-    $sql = "SELECT DM.ten_danh_muc, COUNT(*) AS so_luong_san_pham 
+    $sql = "SELECT DM.ten_danh_muc, COUNT(SP.san_pham_id) AS so_luong_san_pham 
     FROM danh_muc AS DM
-    INNER JOIN san_pham AS SP ON SP.san_pham_id = DM.san_pham_id
+    LEFT JOIN san_pham AS SP ON SP.danh_muc_id = DM.danh_muc_id
     GROUP BY DM.ten_danh_muc";
     return pdo_query($sql);
 }
